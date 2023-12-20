5913f7411a29efed49e0b169cafdb715b6933409
apple <apple@appleui-iMac.local>
2023-12-20 12:50:00 +0900

    version 0.0.1

---
+++ custom.js
@@ -0,0 +1,9 @@
+const img = document.querySelector("img");
+
+// https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/0001.jpg
+
+const a = 10;
+
+const b = a.toString().padStart(4, "0");
+
+console.log(b);
